df32f43ce40b24b287072c75f527e400dc74bd20
MladoniSzabi <szabimlado@gmail.com>
2023-03-31 13:24:28 +0100

    created a _document script and moved the link elements to the head tag inside of it to stop a nextjs error. changed the href to the favicon. moved the favicon to the images folder.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import Head from 'next/head';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import './style.css'
 
@@ -12,19 +12,8 @@ function MainPage({ Component, pageProps }: AppProps) {
             </title>
             <meta charSet="UTF-8"></meta>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-            <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png"></link>
-
-            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''></link>
-            <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@500;700;800&family=League+Spartan:wght@700&display=swap" rel="stylesheet"></link>
         </Head>
-
-        <Component {...pageProps} />
-
-        <div className="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-            Coded by Szabolcs Mladoniczky.
-        </div>
+        <Component {...pageProps}></Component>
     </>
 }
 
