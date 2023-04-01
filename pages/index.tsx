5213fe7c4dca5dc2e54c3918d67bf966a56aa72b
MladoniSzabi <szabimlado@gmail.com>
2023-04-01 22:32:42 +0100

    made the calculator work better on mobile.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,15 @@ import Calculator from '../components/Calculator';
 import ThemeSelector from '../components/ThemeSelector';
 
 function IndexPage() {
-    return <main>
-        <div>
-            <span>calc</span>
-            <ThemeSelector></ThemeSelector>
-        </div>
-        <Calculator></Calculator>
-    </main>
+    return <div className='main-container'>
+        <main>
+            <div>
+                <span>calc</span>
+                <ThemeSelector></ThemeSelector>
+            </div>
+            <Calculator></Calculator>
+        </main>
+    </div>
 }
 
 export default IndexPage;
